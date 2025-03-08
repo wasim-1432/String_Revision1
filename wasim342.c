f795434a73188db92573e5dba4d2154aa8786126
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-08 11:28:32 +0530

    Write s C program to find the first occurrence of given character in a given string.

---
+++ wasim342.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[30],ch;
+    printf("Enter any string\n");
+    fgets(str,30,stdin);
+    str[strlen(str)-1]='\0';
+    printf("Enter any character\n");
+    scanf("%c",&ch);
+    int i;
+    for(i=0;str[i];i++)
+    {
+        if(str[i]==ch)
+        {
+            printf("Index is=%d",i);
+            break;
+        }
+    }
+    if(str[i]!=ch)
+    {
+        printf("Not found");
+    }
+    printf("\n");
+    return 0;
+}
