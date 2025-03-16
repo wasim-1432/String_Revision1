d2c71a34ff5c2478b1322f2c54da78094f76d1d8
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-16 18:51:55 +0530

    Write a C program to sort 10 city names stored in two dimensional arrays,taken from the user.

---
+++ wasim403.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+char *Sort_10_City_Names_Stored_In_Two_Dimensional_Arrays_Taken_From_The_User(char str[10][100]);
+int main()
+{
+    char str[10][100];
+    printf("Enter any 10 cities names\n");
+    Sort_10_City_Names_Stored_In_Two_Dimensional_Arrays_Taken_From_The_User(str);
+    printf("\n");
+    return 0;
+}
+char *Sort_10_City_Names_Stored_In_Two_Dimensional_Arrays_Taken_From_The_User(char str[10][100])
+{
+    int i,l;
+    for(i=0;i<10;i++)
+    {
+        fgets(str[i],100,stdin);
+        for(l=0;str[i][l];l++);
+        str[i][l-1]='\0';
+    }
+    int round;
+    char temp[100];
+    for(round=1;round<10-1;round++)
+    {
+        for(i=0;i<=10-round-1;i++)
+        {
+            if(strcmp(str[i],str[i+1])>0)
+            {
+                strcpy(temp,str[i]);
+                strcpy(str[i],str[i+1]);
+                strcpy(str[i+1],temp);
+            }
+        }
+    }
+    printf("\nRequired Answer is:\n");
+    for(i=0;i<10;i++)
+    {
+        printf("%s\n",str[i]);
+    }
+}
